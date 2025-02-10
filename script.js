bc4112975e37b8b4bf20465acd4f5e789b64837b
itssoumodip <soumodipddas2005@gmail.com>
2025-02-10 22:05:15 +0530

    making the logic

---
+++ script.js
@@ -0,0 +1,27 @@
+const choices = document.querySelectorAll(".userChoices");
+const userWar = document.getElementById('userWar').querySelector('img');
+const computerWar = document.getElementById('computerWar').querySelector('img');
+const msgBox = document.getElementById('msgBox');
+const scoreBoard = document.getElementById('scoreBoard');
+
+let userScore = 0;
+let computerScore = 0;
+
+const choiceArray = ["rock", "paper", "scissors"];
+const userImgPaths = {
+    "rock": "images/1L.png",
+    "paper": "images/2L.png",
+    "scissors": "images/3L.png"
+}
+const computerImgPaths = {
+    "rock": "images/1R.png",
+    "paper": "images/2R.png",
+    "scissors": "images/3R.png"
+}
+
+choices.forEach((choice, index) => {
+    choice.addEventListener("click", () => {
+        userChoice = choiceArray[index];
+        userWar.src = userImgPaths[]
+    })
+});

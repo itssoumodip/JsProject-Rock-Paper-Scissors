04d81557d3e64f8d57a53d60e55bbc94a2e27392
itssoumodip <soumodipddas2005@gmail.com>
2025-02-11 18:25:38 +0530

    complet this project

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const userWar = document.getElementById('userWar').querySelector('img');
 const computerWar = document.getElementById('computerWar').querySelector('img');
 const msgBox = document.getElementById('msgBox');
 const scoreBoard = document.getElementById('scoreBoard');
+const resultBtn = document.getElementById('restratBtn');
 // const warGoing = document.getElementsByClassName('warGoing').querySelector('img')
 
 let userScore = 0;
@@ -21,8 +22,9 @@ const computerImgPaths = {
 }
 
 choices.forEach((choice, index) => {
-    choice.addEventListener("click", () => {
-
+    choice.addEventListener("click", () => { 
+        userWar.src = userImgPaths["rock"];
+        computerWar.src = computerImgPaths["rock"];
         userWar.classList.add('handMotion');
         computerWar.classList.add('handMotion');
         msgBox.textContent = "Loading....";   
@@ -33,11 +35,47 @@ choices.forEach((choice, index) => {
             const computerChoice = choiceArray[Math.floor(Math.random()*3)];
             userWar.src = userImgPaths[userChoice];
             computerWar.src = computerImgPaths[computerChoice];
+            const result = winnerDecide(userChoice, computerChoice);
+            updateMesg(result);
+            updateScore(result);
         },2000)
     })
 });
-function winnerDecide() {
-    
+function winnerDecide(user, computer) {
+    if (user===computer) 
+        return "draw";
+    else if (
+        (user==="rock" && computer==="scissors") ||
+        (user==="paper" && computer==="rock") ||
+        (user==="scissors" && computer==="paper")
+    )
+        return "win";
+    else 
+        return "lost";
 }
-
-
+function updateMesg(result) {
+    if (result === "win")
+        msgBox.textContent = "You Win";
+    else if (result === "lost") 
+        msgBox.textContent = "You Loss !!";
+    else 
+        msgBox.textContent = "It's a Draw!";
+}
+function updateScore(result) {
+    if (result==="win") 
+        userScore++;
+    if (result==="lost")
+        computerScore++;
+    scoreBoard.textContent = `User : ${userScore} | CPU : ${computerScore}`;
+    scoreBoard.style.display = "block";
+    resultBtn.style.display = "block";
+}
+function restrat () {
+    userScore = 0;
+    computerScore = 0;
+    msgBox.textContent = "Let's Play !!";   
+    userWar.src = userImgPaths["rock"];
+    computerWar.src = computerImgPaths["rock"];
+    scoreBoard.style.display = "none";
+    resultBtn.style.display = "none";
+}
